2e8bc16ff900453c5e5539b255b790dd3716ed85
chinmayk1613 <50278095+chinmayk1613@users.noreply.github.com>
2021-02-11 01:13:13 +0100

    Update myModel.py

--- myModel.py
+++ myModel.py
@@ -17,16 +17,6 @@ def trainLinear():
     from sklearn.model_selection import train_test_split
     X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
 
-
-    #feature scaling
-    """from sklearn.preprocessing import StandardScaler
-    sc_X=StandardScaler()
-    X_train=sc_X.fit_transform(X_train)
-    X_test=sc_X.transform(X_test)
-    sc_y=StandardScaler()
-    y_train=sc_y.fit_transform(y_train)"""
-    #scalling the dummy coulmn depends interpreation of model and it is depends
-
     regressor.fit(X_train,y_train)  #ft is method name which fit the regreesor to data
     pickle.dump(regressor, open('myModel.pkl', 'wb'))
 def prediction():
